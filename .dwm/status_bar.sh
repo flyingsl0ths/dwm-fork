dc1d35b2f29641cf7cc5c0b3af20e37fc3b669f2
flyingsl0ths <randomemail33110@protonmail.com>
2021-10-18 21:37:26 -0500

    Added volume level to status bar

--- .dwm/status_bar.sh
+++ .dwm/status_bar.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 status_bar() {
     ram_usage="[R: $(free -h --si | grep Mem | awk '{print ($3)}')]"
 
     todays_date="[D: $(date +"%b %d, %Y %a %I:%M%p")]"
 
-    battery_level="[B: $(cat /sys/class/power_supply/BAT1/capacity)]"
+    battery_level="[B: $(cat /sys/class/power_supply/BAT1/capacity)%]"
 
-    xsetroot -name "$ram_usage | $todays_date | $battery_level"
+    volume_level="[V: $(pamixer --get-volume)]"
+
+    xsetroot -name "$ram_usage | $todays_date | $battery_level | $volume_level"
 }
 
 while true; do

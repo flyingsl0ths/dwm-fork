3e4a5a4f8e181c312132d80880072a8357a9d16e
flyingsl0ths <randomemail32110@gmail.com>
2022-07-13 00:04:43 -0500

    Tidy up status bar script

--- .dwm/status_bar.sh
+++ .dwm/status_bar.sh
@@ -1,30 +1,30 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 kill_existing() {
-    instance_pid="$(pgrep -fo "/bin/sh /home/flyingsl0ths/.dwm/status_bar.sh")"
+  instance_pid="$(pgrep -fo "bash $HOME/.dwm/status_bar.sh &")"
 
-    if [ "$instance_pid" != "$$" ]; then
-        kill -KILL "$instance_pid"
-    fi
+  if [ "$instance_pid" != "$$" ]; then
+    kill -KILL "$instance_pid"
+  fi
 }
 
 status_bar() {
-    wifi_connection="[^c#ABE9B3^  ^c#D9E0EE^$(nmcli -t -f name connection show --active | awk '{print $1}')]"
+  wifi_connection="[^c#ABE9B3^  ^c#D9E0EE^$(nmcli -t -f name connection show --active | awk '{print $1}')]"
 
-    ram_usage="[^c#F8BD96^ ^c#D9E0EE^$(free -h --si | grep Mem | awk '{print ($3)}')]"
+  ram_usage="[^c#F8BD96^ ^c#D9E0EE^$(free -h --si | grep Mem | awk '{print ($3)}')]"
 
-    todays_date="[^c#E8A2AF^  ^c#D9E0EE^$(date +"%b %d, %Y %a %I:%M%p")]"
+  todays_date="[^c#E8A2AF^  ^c#D9E0EE^$(date +"%b %d, %Y %a %I:%M%p")]"
 
-    battery_level="[^c#B5E8E0^  ^c#D9E0EE^$(cat /sys/class/power_supply/BAT1/capacity)%]"
+  battery_level="[^c#B5E8E0^  ^c#D9E0EE^$(cat /sys/class/power_supply/BAT1/capacity)%]"
 
-    volume_level="[^c#C9CBFF^ ^c#D9E0EE^$(pamixer --get-volume-human)]"
+  volume_level="[^c#C9CBFF^ ^c#D9E0EE^$(pamixer --get-volume-human)]"
 
-    xsetroot -name "$wifi_connection $ram_usage $todays_date $battery_level $volume_level"
+  xsetroot -name "$wifi_connection $ram_usage $todays_date $battery_level $volume_level"
 }
 
 kill_existing
 
 while true; do
-    status_bar
-    sleep 0.5s
+  status_bar
+  sleep 0.5s
 done
